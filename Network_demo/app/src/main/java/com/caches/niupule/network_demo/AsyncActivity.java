51064affd3dd01510cc917b90bf6f637ffdf07e6
paulniu <niupuyue@aliyun.com>
2018-02-04 01:47:03 +0800

    网络请求例子开始，测试本地提交

---
+++ Network_demo/app/src/main/java/com/caches/niupule/network_demo/AsyncActivity.java
@@ -0,0 +1,13 @@
+package com.caches.niupule.network_demo;
+
+import android.app.Activity;
+
+/**
+ * Created: niupule
+ * Date: 2018/2/4  上午1:43
+ * E-mail:niupuyue@aliyun.com
+ * des:
+ */
+
+public class AsyncActivity extends Activity{
+}
--- Network_demo/app/src/main/java/com/caches/niupule/network_demo/MainActivity.java
+++ Network_demo/app/src/main/java/com/caches/niupule/network_demo/MainActivity.java
@@ -1,13 +1,39 @@
 package com.caches.niupule.network_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btn01,btn02,btn03;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn01 = findViewById(R.id.btn01);
+        btn01.setOnClickListener(this);
+
+        btn02 = findViewById(R.id.btn02);
+        btn02.setOnClickListener(this);
+
+        btn03 = findViewById(R.id.btn03);
+        btn03.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn01){
+            startActivity(new Intent(MainActivity.this,AsyncActivity.class));
+        }else if(view.getId() == R.id.btn02){
+            startActivity(new Intent(MainActivity.this,OKHttpActivity.class));
+        }else if(view.getId() == R.id.btn03){
+            startActivity(new Intent(MainActivity.this,RetrofitActivity.class));
+        }
     }
 }
