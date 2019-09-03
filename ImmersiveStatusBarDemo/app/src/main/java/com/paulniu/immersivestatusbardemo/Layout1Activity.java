7f738169c44760d53b268823c44e740277ef5b25
paulniu <niupuyue@aliyun.com>
2019-09-03 19:30:36 +0800

    add code

---
+++ ImmersiveStatusBarDemo/app/src/main/java/com/paulniu/immersivestatusbardemo/Layout1Activity.java
@@ -0,0 +1,42 @@
+package com.paulniu.immersivestatusbardemo;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Coder: niupuyue
+ * Date: 2019/9/3
+ * Time: 19:11
+ * Desc:
+ * Version:
+ */
+public class Layout1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_1);
+        // 隐藏状态栏和ActionBar
+//        View decorView = getWindow().getDecorView();
+//        int option = View.SYSTEM_UI_FLAG_FULLSCREEN;
+//        decorView.setSystemUiVisibility(option);
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.hide();
+
+        // 另一种效果
+        if (Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
--- ImmersiveStatusBarDemo/app/src/main/java/com/paulniu/immersivestatusbardemo/MainActivity.java
+++ ImmersiveStatusBarDemo/app/src/main/java/com/paulniu/immersivestatusbardemo/MainActivity.java
@@ -2,13 +2,26 @@ package com.paulniu.immersivestatusbardemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn01 = findViewById(R.id.btn01);
+        btn01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Layout1Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
